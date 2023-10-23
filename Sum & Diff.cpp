3b4473e3bbf88afa754d10b8d3ba673b908d3c46
Blacky <97872836+DrBlack22@users.noreply.github.com>
2023-10-23 22:20:24 +0500

    Fïrst commit

---
+++ Sum & Diff.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int f;
+    int h;
+    int diff;
+    cout << "yes, fren? \n";
+    cin >> f;
+    cout << "yes, my fren? \n";
+    cin >> h;
+    diff = f - h;
+    cout << "the diff my fren, is " << diff << " !" << endl;
+    return 0;
+}
